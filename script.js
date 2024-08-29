a71ab206aa081f465daa530b65f72d69f7d69f9d
InAweofMyTism <inaweofmytism@gmail.com>
2024-08-29 08:03:19 -0400

    add JS pseudocode

---
+++ script.js
@@ -0,0 +1,6 @@
+/* 
+1. add 16 32px x 32px white squares to grid on load
+2. add event listener to each square 
+3. color square upon activation of event listener
+4. reset (change all color backgrounds to white) when button is clicked
+*/
