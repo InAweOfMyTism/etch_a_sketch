5b9ae686ce867255f59e4c24cef1c29b78a83a04
InAweofMyTism <inaweofmytism@gmail.com>
2024-08-29 08:16:51 -0400

    add more pseudocode to JS file

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 /* 
 1. add 16 32px x 32px white squares to grid on load
-2. add event listener to each square 
-3. color square upon activation of event listener
-4. reset (change all color backgrounds to white) when button is clicked
+    1a. get sketch area element from doc
+    1b. divide sketch area size by 4 to get square size
+    1c. loop 0-15 
+        i. create each square
+        ii. add the event listener to each square for hover to turn it black
+        iii. append square as a child of sketch 
+    1d.
+2. reset (change all color backgrounds to white) when button is clicked
+    2a. remove all children from sketch area
+    2b. repopulate
 */
+
+//get info
+
+    //get sketch element and button element
+    //divide size of button element by 4 to get square size
+ 
+//create grid
+
+//add listener to reset button
+
+    //remove squares
+    //add new squares
+
+function createGrid() {
+    // loop from 0 to 15
+    //create element
+    //add event listener for hover event
+    //add draw function to EL
+    
+}
+
+function draw() {
+    //change background color to black
+}
+
+function clear() {
+    //remove all squares
+}
