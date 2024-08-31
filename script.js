bd79fade1a79d9c662679a5540d5ac4c72117c05
InAweofMyTism <inaweofmytism@gmail.com>
2024-08-31 13:20:29 -0400

    enter terrible commit message here

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
+const container = document.getElementById('container');
+const sketchArea = document.getElementById('sketchArea');
 
-const container = document.getElementById('container')
-const sketch_width = 512;
-const sketch_height = 512;
-const side_length = 4;
-const square_width = sketch_width / side_length;
-const square_height = sketch_height / side_length;
-
-
-createGrid();
+createGrid(4);
 createResetButton();
 
+function createSketchPad(size) {
 
-
-function createGrid() {
-    
-    let sketchArea = document.createElement('div');
     sketchArea.setAttribute('id', 'sketchArea');
+    sketchArea.style.width = `${size}px`;
+    sketchArea.style.height = `${size}px`; 
 
-    sketchArea.style.width = sketch_width;
-    sketchArea.style.height = sketch_height;
+
+}
+
+function createGrid(side_length) {
+
+    
+
+    let square_width = side_length;
+    let square_height = side_length;
+
+
     
     for (let row = 0; row < side_length; row++){
         for (let col = 0; col < side_length; col++){
             let newSquare = document.createElement('div');
             newSquare.classList.add('pixel');
-            newSquare.style.width = square_width;
-            newSquare.style.height = square_height;
+            newSquare.style.width = `${square_width}px`;
+            newSquare.style.height = `${square_height}px`;
             newSquare.addEventListener('mouseover', (event) => {
                 event.target.style.backgroundColor = 'black';
             });
@@ -33,7 +35,6 @@ function createGrid() {
         }
     }
 
-    container.appendChild(sketchArea);
 
 
 }
@@ -43,8 +44,17 @@ function createResetButton() {
     resetButton.setAttribute('id', 'resetButton');
 
     resetButton.innerText = 'RESET';
+    resetButton.addEventListener('click', reloadGrid(prompt('enter new no. of squares')))
 
     container.appendChild(resetButton);
+}
 
+function reloadGrid(side_length) {
+
+    if (side_length <= 100) {
+        createGrid(side_length);
+    } else {
+        reload(prompt('enter a number less than 100'));
+    }
 
 }
